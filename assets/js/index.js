4bfa069c3bcc718c86ed35f8e9c2851cbdc09af8
chikiza <83772028+chikiza@users.noreply.github.com>
2024-07-05 04:25:14 -0400

    my chart
    no funciona

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,25 +14,23 @@ btnCat.addEventListener('click', () =>{
     .catch(error => console.log(error))
 })
 
-const createChart = () => {
-    const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'polarArea',
+const myChart = new Chart(ctx, {
+    type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
         label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
         borderWidth: 1
-      }]
+    }]
     },
     options: {
-      scales: {
+        scales: {
         y: {
-          beginAtZero: true
+            beginAtZero: true
         }
-      }
     }
-  });
-}
+    } 
+    });
